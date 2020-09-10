c620d80c08e2cec6e2aec47c0fbba746db3805cb
tveijola <tommi.veijola@gmail.com>
2020-09-10 14:57:18 +0300

    configured platform specific font

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,5 @@
+import { Platform } from "react-native";
+
 const theme = {
   colors: {
     textPrimary: '#24292e',
@@ -14,7 +16,11 @@ const theme = {
     appBarTab: 18
   },
   fonts: {
-    main: 'System',
+    main: Platform.select({
+      android: 'Roboto',
+      ios: 'Arial',
+      default: 'System'
+    }),
   },
   fontWeights: {
     normal: '400',
